df4426a1222e1f04b8d2301f023fc1e7fbc3f8ac
lautaro anazgo <lautaroanazgo@gmail.com>
2025-10-24 14:32:10 -0300

    git ignore

--- tp7/obj/Debug/net9.0/tp7.AssemblyInfo.cs
+++ tp7/obj/Debug/net9.0/tp7.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tp7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dc55a069f55c8bf77e6d9ec4580481f1f08fcec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff22f9d5d2f8d5c86e44672d064f843edabb5fb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("tp7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tp7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
